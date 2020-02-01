037ebca771cd8743660c396d3f98e503e4d69c3f
Nithiyanandam <Karthikeyan.Nithiyanandam@rabobank.nl>
2020-02-01 15:07:05 +0530

    Added detailed ReadMD file.

--- src/main/java/com/techstack/demo/vo/Address.java
+++ src/main/java/com/techstack/demo/vo/Address.java
@@ -6,13 +6,10 @@ import lombok.Data;
 @Data @Builder
 public class Address {
 
-    private String id;
-    private String allotmentNumber;
     private CountryCode countryCode;
     private String cityName;
     private int houseNumber;
     private String houseNumberExtension;
-    private boolean inactiveFlag;
     private String postalAddressName;
     private String postalCode;
     private String streetAddress;
--- src/main/java/com/techstack/demo/vo/AddressVO.java
+++ src/main/java/com/techstack/demo/vo/AddressVO.java
@@ -1,14 +1,10 @@
 package com.techstack.demo.vo;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
-@JsonIgnoreProperties(ignoreUnknown = true)
 @Data @Builder
 public class AddressVO implements Serializable {
     private static final long serialVersionUID = -822837896213811708L;
